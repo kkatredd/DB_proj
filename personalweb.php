697550076c88241d311ef1734a3dd873791cc048
kkatredd <karthiktejacool@gmail.com>
2019-03-07 18:20:06 -0500

    first

---
+++ personalweb.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Karthik Website</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+.header {
+  padding: 100px;
+  text-align: center;
+}
+.header h1 {
+  font-size: 40px;
+}
+</style>
+</head>
+<body>
+<div class="header">
+<h1>
+<center><?php echo "This is Karthik Teja's page";?>
+</center>
+</h1>
+</body>
+</html>
